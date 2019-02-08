24374ea974de62e868c7c61812c697cc274d411b
Christopher Arnold <chrisarnold2010@gmail.com>
2019-02-08 11:04:30 -0600

    added sum

--- practice.js
+++ practice.js
@@ -310,6 +310,25 @@ function printNumber(num) {
     }
 }
 
+// add array of numbers
+
+function add(array) {
+    let total = 0;
+    let length = array.length;
+    for (let i =0; i < length; i++) {
+        total+=array.pop()
+    }
+    return total;
+}
+
+function add(array) {
+    let total = 0;
+    for (let i =0; i < array.length; i++) {
+        total+=array[i]
+    }
+    return total;
+}
+
 
 
 // function palindrome(string) {
