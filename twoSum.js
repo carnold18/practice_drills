246c1fc0a47f04bacc500a1d52f4ccb5dd6b1366
Christopher Arnold <chrisarnold2010@gmail.com>
2019-02-17 12:49:58 -0600

    added find all indices for multiple of same value

--- twoSum.js
+++ twoSum.js
@@ -2,13 +2,6 @@ var twoSum = function(nums, target) {
     let array = [];
     let index1 = 0;
     let index2 = 0;
-    let findAllIndices = function(arr, val) {
-        var indexes = [], k;
-        for(k = 0; k < arr.length; k++)
-            if (arr[k] === val)
-                indexes.push(k);
-        return indexes;
-    }
     for (let i = 0; i < nums.length; i++) {
         for (let j = 0; j < nums.length-1; j++) {
             if (nums[i]+nums[j+1] === target) {
@@ -17,13 +10,28 @@ var twoSum = function(nums, target) {
                 index1+=nums[i];
                 index2+=nums[j+1];
                 if (nums[i] === nums[j+1]) {
-                    findAllIndices();
+                    console.log('the index values are the same');
+                    console.log('so, i am invoking the findAllIndices function');
+                    var findAllIndices = function(arr, val) {
+                        let indexes = []
+                        for(let k = 0; k < arr.length; k++)
+                            if (arr[k] === val) {
+                                indexes.push(k);
+                            }
+                            // console.log('indexes of same index value array:', indexes);
+                            // console.log('master array:', array)
+                        // return indexes;
+                        array = [...indexes]
+                        console.log('indexes of same index value array:', indexes);
+                        console.log('master array:', array)
+                    }
+                    findAllIndices(nums, nums[i]);
+                    return array;
                 } else {
                     array.push(nums.indexOf(index1));
                     array.push(nums.indexOf(index2));
                     return array;
                 }
-                
             }
         }
     }
